b4937a4b236f337678c0ed7d8cf9e12460427bf7
Bri8102 <damioniru16@gmail.com>
2021-01-15 01:16:08 +0100

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
   render() {
+
+    const {restaurants, deleteRestaurant} = this.props
+    const restaurantList = restaurants.map(restaurant => 
+      <Restaurant restaurant={restaurant} key={restaurant.id} deleteRestaurant={deleteRestaurant}/>
+    )
+
     return(
       <ul>
-        Restaurants Component
+         {restaurantList}
       </ul>
     );
   }
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,17 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+
+
+    const { reviews, restaurantId, deleteReview } = this.props
+    const parentRestaurant = reviews.filter(review => review.restaurantId === restaurantId)
+    const reviewList = parentRestaurant.map((review, index) => 
+      <Review key={index} review={review} deleteReview={deleteReview}/>
+      ) 
+
     return (
       <ul>
-        Reviews
+        {reviewList}
       </ul>
     );
   }
